ce891e4074607336c1771ba474e430d2ac92c992
demakk <dimademch03@gmail.com>
2023-07-27 14:03:37 +0300

    h

---
+++ DatingApp.Api/Controllers/UsersController.cs
@@ -0,0 +1,34 @@
+﻿using DatingApp.Api.Data;
+using DatingApp.Api.Entities;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using SQLitePCL;
+
+namespace DatingApp.Api.Controllers;
+
+[ApiController]
+[Route("[controller]")]
+public class UsersController : ControllerBase
+{
+    private readonly DataContext _ctx;
+    
+    public UsersController(DataContext ctx)
+    {
+        _ctx = ctx;
+    }
+
+    [HttpGet]
+    public async Task<IActionResult> GetUsers()
+    {
+        var users = await _ctx.Users.ToListAsync();
+        return Ok(users);
+    }
+
+    [HttpGet]
+    [Route("{id}")]
+    public async Task<IActionResult> GetUserById(int id)
+    {
+        var user = await _ctx.Users.FirstOrDefaultAsync(u => u.Id == id); 
+        return Ok(user);
+    }
+}
---
+++ DatingApp.Api/Program.cs
@@ -0,0 +1,28 @@
+using DatingApp.Api.Data;
+using Microsoft.EntityFrameworkCore;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+
+builder.Services.AddControllers();
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddDbContext<DataContext>(options =>
+{
+    options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+}
+
+app.UseHttpsRedirection();
+
+app.UseAuthorization();
+
+app.MapControllers();
+
+app.Run();
